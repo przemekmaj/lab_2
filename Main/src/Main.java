dee10007dfd9febdf949a1dc6ba653bca215c451
przemekmaj <majorek2509@gmail.com>
2022-01-29 17:07:01 +0000

    Zad 1.1

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int liczba=1000;
+        do{
+            liczba--;
+            System.out.println(liczba);
+            if(liczba%78==0) System.out.println("Liczba "+liczba+" jest podzielna przez 78");
+        }
+        while(liczba!=0);
+        //nr indeksu 58178
     }
 }
 
