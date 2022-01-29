cd343923442d2d203d75773662dd2b94a48b18ff
przemekmaj <majorek2509@gmail.com>
2022-01-29 17:38:54 +0000

    1

---
+++ Main/src/Zad2_1.java
@@ -0,0 +1,8 @@
+public class Zad2_1 {
+    public static void main(String[] args) {
+        for(int i = 0;i<=100;i++){
+            System.out.println(i);
+            if(i%8==0)System.out.println("Liczba "+i+" jest podzielna przez 8");
+        }
+    }
+}
